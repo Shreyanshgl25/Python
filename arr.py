982a47d10d5b92cfb7e45fe6e85b03880c9b13e3
Shreyansh Goyal <110629672+Shreyanshgl25@users.noreply.github.com>
2025-02-21 10:51:25 +0530

    Update arr.py

--- arr.py
+++ arr.py
@@ -1,6 +1,8 @@
 #array code
 import array
 
+
+a=print
 a1=array.array('i',[1,2,3,4,5,4,5,6,3,6,7,8])
 a1.append(6)
 a1.remove(6)
@@ -8,5 +10,7 @@ a1.remove(6)
 n=len(a1)
 for i in range(n):
     print(a1[i], end=' ,')
+
+print(a)
 
 
